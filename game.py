55c6881ade7b0dc59b84698e19e7999244a0fe40
Eduardo Pereira de Queiroz <eduardopq4@gmail.com>
2024-10-09 22:17:39 -0300

    feat: starting project,  adding classes

---
+++ game.py
@@ -0,0 +1,13 @@
+import os
+os.system("cls")
+
+# CRIANDO O USUÁRIO
+print("=== CRIE SEU PERSONAGEM: \n")
+nome = str(input("-- Insira o nome do seu personagem: "))
+print("=== ESCOLHA SUA CLASSE: \n")
+print("=== 1 - GUERREIRO: Força = 15 / Carisma = 5 / Agilidade = 10 / Inteligência = 7 / Magia = 0")
+print("=== 2 - LADINO: Força = 5 / Carisma = 7 / Agilidade = 15 / Inteligência = 10 / Magia = 5")
+print("=== 3 - MAGO: Força = 0 / Carisma = 10 / Agilidade = 7 / Inteligência = 15 / Magia = 15")
+print("=== 4 - BARDO: Força = 5 / Carisma = 15 / Agilidade = 10 / Inteligência = 7 / Magia = 10")
+
+    
